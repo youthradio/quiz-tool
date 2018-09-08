3042755221b3e5b6cd21287c35b45ce0b9d423bf
Radames Ajna <radamajna@gmail.com>
2018-09-07 17:15:11 -0700

    fetching data from graphql server

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,58 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+const QUERY = (id) => `
+query {
+  quiz: getQuizById( id: "${id}" ){
+    id
+    description
+    featureImage
+    questions {
+      id
+      questionOrder
+      questionText
+      featureImage
+      options{
+        id
+        optionText
+        resultText
+        correctOption
+      }
+    }
+  }
+}
+`
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      data : null,
+      loading: false
+    }
+  },
+  created() {
+    this.makeGraphQLQuery('http://localhost:4000/graphql', QUERY('5b930f393f9082129af6bc74'))
+  },
+  methods: {
+      async  makeGraphQLQuery (serverURL, query) {
+        this.loading = true;
+        const graphqlFetch = async () => {
+            const res = await fetch(serverURL, {
+              headers: { 'Content-Type': 'application/json' },
+              referrer: "no-referrer", // no-referrer, *client
+              method: 'POST',
+              body: JSON.stringify({ query })
+            });
+          return res;
+        }
+        const fetchedData = await graphqlFetch();
+        const data = await fetchedData.json();
+        this.data = data.data;
+        this.loading = false;
+    }
   }
 }
 </script>
