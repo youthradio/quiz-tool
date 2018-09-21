1606452f2f8681feb14bff71d4a5d7bc3537860f
Radames Ajna <radamajna@gmail.com>
2018-09-20 17:32:12 -0700

    change font size to match new platform

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     id="app"
     class="container-fluid">
     <div class="row">
-      <div class="col-12 col-md-6 mx-auto">
+      <div class="col-12 col-md-10 mx-auto">
         <QuizFront/>
       </div>
     </div>
